0bc3d1736c7ef32cfcaeeca192a82a1823af289d
Tamziy <tamziyafolabi@gmail.com>
2026-09-17 08:20:33 -0500

    feat(application): implement ticket use cases

--- src/ticket/application/ticket.service.ts
+++ src/ticket/application/ticket.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { TicketEntity, TicketSeverity } from '../domain/ticket.entity';
+import { ITicketRepository } from '../domain/ticket.repository.interface';
 
-@Injectable()
-export class TicketService {}
+export class TicketService {
+  private repository: ITicketRepository;
+
+  constructor(repo: ITicketRepository) {
+    this.repository = repo;
+  }
+
+  createTicket(
+    id: string,
+    title: string,
+    description: string,
+    severity: TicketSeverity,
+  ): void {
+    const theTicket = new TicketEntity(id, title, description, severity);
+    this.repository.save(theTicket);
+  }
+
+  assignTicket(id: string, userId: string): void {
+    const foundTicket = this.repository.getById(id);
+    if (foundTicket) {
+      foundTicket.assign(userId);
+      this.repository.save(foundTicket);
+    }
+  }
+
+  startTicket(id: string): void {
+    const foundTicket = this.repository.getById(id);
+    if (foundTicket) {
+      foundTicket.startWork();
+      this.repository.save(foundTicket);
+    }
+  }
+
+  resolveTicket(id: string): void {
+    const foundTicket = this.repository.getById(id);
+    if (foundTicket) {
+      foundTicket.resolve();
+      this.repository.save(foundTicket);
+    }
+  }
+}
